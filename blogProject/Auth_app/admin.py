152724ddcad328223ab91cf6b38bc96255035e91
Jay <jay@gmail.com>
2024-09-04 11:36:45 -0400

    Day 2 Session 1

--- blogProject/Auth_app/admin.py
+++ blogProject/Auth_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models 
 # Register your models here.
+
+# admin.site.register(models.Contact)
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'email', 'mobile_number',"message")
